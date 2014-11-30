975151229bbb9e33a8b539d46f3327f0d5ee9e18
Seth Lakowske <lakowske@gmail.com>
2014-11-30 11:05:13 -0600

    Create a Writable tester

---
+++ WriteTest.js
@@ -0,0 +1,32 @@
+/*
+ * WriteTest checks each write chunk against the expected write chunk
+ * and throws error messages if they are not strictly equal.
+ *
+ * (C) Seth Lakowske
+ */
+function WriteTest(options) {
+    if (!(this instanceof WriteTest)) {
+        return new WriteTest(options);
+    }
+
+    this.test = options.test;
+    this.testChunks = options.testChunks;
+    this.pos = 0;
+    Writable.call(this, options);
+}
+util.inherits(WriteTest, Writable);
+
+WriteTest.prototype._write = function(chunk, enc, callback) {
+    var testChunk = this.testChunks[this.pos];
+    var chunkString = chunk.toString('utf8');
+    var same = testChunk === chunkString;
+    if (same) {
+        console.log(this.pos + ' ' + chunkString + ' equaled ' + testChunk)
+    } else {
+        console.log(this.pos + ' ' + chunkString + ' did not equal ' + testChunk)
+    }
+
+    this.test.strictEqual(chunkString, testChunk, 'chunks should be equal');
+    this.pos += 1;
+    callback();
+}
